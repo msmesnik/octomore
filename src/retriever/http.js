c125eb06ae703b38880b8e67327e3a41e73fedcc
Michael Smesnik <ms@tailored-apps.com>
2017-02-04 20:31:59 +0100

    implement basic http retriever

--- src/retriever/http.js
+++ src/retriever/http.js
@@ -1,6 +1,10 @@
 'use strict'
 
-import getDebugger from 'debug'
+// import getDebugger from 'debug'
+import request from 'request-promise'
 
-const debug = getDebugger('octomore:retriver:http')
+// const debug = getDebugger('octomore:retriver:http')
 
+export default function createHttpRetriever (defaults = { }) {
+  return async (uri, options = { }) => await request({ ...defaults, ...options, uri })
+}
