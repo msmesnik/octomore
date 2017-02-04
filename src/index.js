dd1574f4e97d886c15292be7bb5cd8ae915fd5d7
Michael Smesnik <ms@tailored-apps.com>
2017-02-04 20:32:21 +0100

    remove babel-polyfill dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-require('babel-polyfill')
-
 import createDocument from './document'
 import createTransformer from './transformer'
 import createHttpRetriever from './retriever/http'
