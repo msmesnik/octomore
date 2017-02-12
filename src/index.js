ae91d4d21a668c0913dc3c10ffdc994ded711ee6
Michael Smesnik <ms@tailored-apps.com>
2017-02-12 22:36:11 +0100

    add caches to exports of main file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 'use strict'
 
 import createDocument from './document'
+import createFileCache from './cache/file'
 import createFileRetriever from './retriever/file'
 import createHttpRetriever from './retriever/http'
+import createPseudoCache from './cache/pseudo'
 import createTransformer from './transformer'
 
-export { createDocument, createFileRetriever, createHttpRetriever, createTransformer }
+export {
+  createDocument,
+  createFileCache,
+  createFileRetriever,
+  createHttpRetriever,
+  createPseudoCache,
+  createTransformer
+}
