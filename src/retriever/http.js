5e2e1b8444f92030912469bad395c08c24085492
Michael Smesnik <ms@tailored-apps.com>
2017-02-17 10:48:50 +0100

    remove comment litter

--- src/retriever/http.js
+++ src/retriever/http.js
@@ -1,9 +1,6 @@
 'use strict'
 
-// import getDebugger from 'debug'
 import request from 'request-promise'
-
-// const debug = getDebugger('octomore:retriver:http')
 
 export default function createHttpRetriever (defaults = { }) {
   return async (uri, options = { }) => await request({ ...defaults, ...options, uri })
