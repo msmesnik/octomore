25b9325175e8a7e89ed037e0335b069085db97b9
Michael Smesnik <ms@tailored-apps.com>
2017-02-21 16:25:26 +0100

    add "json" config to file cache

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 2017-02-21
+* `[SEMVER_PATCH]` Added `json` config property to `createFileCache`
+
 ## 2017-02-17
 * `[SEMVER_MINOR]` `createDocument` now provides a `getUri` function by default
 * `[SEMVER_MINOR]` Added `createAdditiveTransformer` function
--- src/cache/file.js
+++ src/cache/file.js
@@ -9,7 +9,7 @@ const debug = getDebugger('octomore:cache:file')
 
 Promise.promisifyAll(fs)
 
-export default function createFileCache ({ lifetime = 0, directory = 'cache', extension = 'json' } = { }) {
+export default function createFileCache ({ lifetime = 0, directory = 'cache', extension = 'json', json = true } = { }) {
   debug('Creating file cache object. Lifetime is %s sec, cache directory is "%s", file extension is "%s".', lifetime, directory, extension)
 
   const getFullPath = (id) => path.resolve(directory, `${id}.${extension}`)
@@ -38,7 +38,7 @@ export default function createFileCache ({ lifetime = 0, directory = 'cache', ex
     debug('Attempting to write cache file %s', fullPath)
 
     await fs.ensureDirAsync(path.dirname(fullPath))
-    await fs.writeFileAsync(fullPath, JSON.stringify(data), { encoding: 'utf8' })
+    await fs.writeFileAsync(fullPath, json ? JSON.stringify(data) : data, { encoding: 'utf8' })
 
     debug('Cache file %s written', fullPath)
   }
@@ -51,7 +51,7 @@ export default function createFileCache ({ lifetime = 0, directory = 'cache', ex
 
     debug('Cache file %s read, JSON.parse()ing contents.', fullPath)
 
-    return JSON.parse(raw)
+    return json ? JSON.parse(raw) : raw
   }
   const remove = async (id) => {
     const fullPath = getFullPath(id)
