11db4f3e4ec62e5e4a6a8a8752ca2d0fc2b8824b
Michael Smesnik <ms@tailored-apps.com>
2017-03-18 16:22:33 +0100

    allow disabling cache by supplying falsey values for rawCache and transformedCache

--- src/document.js
+++ src/document.js
@@ -7,7 +7,7 @@ const createPseudoCache = require('./cache/pseudo')
 const debug = getDebugger('octomore:document')
 const hashString = (str, algorithm = 'md5') => crypto.createHash(algorithm).update(str).digest('hex')
 
-module.exports = function defineDocument ({ retriever, uriTemplate, getUri = (id) => `${id}`, transformer = noTransform, rawCache = createPseudoCache(), transformedCache = createPseudoCache(), getCacheId = hashString, friendlyName = 'Document' }) {
+module.exports = function defineDocument ({ retriever, uriTemplate, getUri = (id) => `${id}`, transformer = noTransform, rawCache = false, transformedCache = false, getCacheId = hashString, friendlyName = 'Document' }) {
   if (typeof retriever !== 'function') {
     throw new Error('A retriever function must be provided when defining a document.')
   }
@@ -16,6 +16,7 @@ module.exports = function defineDocument ({ retriever, uriTemplate, getUri = (id
     throw new Error('You must provide either an "uriTemplate" string or a "getUri" function when creating a new document.')
   }
 
+  const cache = { raw: rawCache || createPseudoCache(), transformed: transformedCache || createPseudoCache() }
   const getFullUri = uriTemplate ? (id) => uriTemplate.replace(/\{id\}/i, id) : getUri
   const isInCache = async (id, cache) => await cache.exists(id) && !(await cache.isOutdated(id))
 
@@ -23,31 +24,31 @@ module.exports = function defineDocument ({ retriever, uriTemplate, getUri = (id
     const debugDoc = (msg, ...params) => debug(`[%s %s] ${msg}`, friendlyName, id, ...params)
     const uri = await getFullUri(id, options)
     const cacheId = await getCacheId(uri)
-    const cachedTransformedData = await isInCache(cacheId, transformedCache)
+    const cachedTransformedData = await isInCache(cacheId, cache.transformed)
 
     if (cachedTransformedData) {
       debug('Returning cached transformed data for uri %s (cache id %s).', uri, cacheId)
 
-      return await transformedCache.retrieve(cacheId)
+      return await cache.transformed.retrieve(cacheId)
     }
 
-    const cachedRawData = await isInCache(cacheId, rawCache)
+    const cachedRawData = await isInCache(cacheId, cache.raw)
 
     debugDoc('Retrieving raw data from %s, full uri is %s (cache id %s)', cachedRawData ? 'cache' : 'source', uri, cacheId)
 
-    const raw = cachedRawData ? await rawCache.retrieve(cacheId) : await retriever(uri, options)
+    const raw = cachedRawData ? await cache.raw.retrieve(cacheId) : await retriever(uri, options)
 
     debugDoc('Raw data retrieved - applying transformation')
 
     if (!cachedRawData) {
-      await rawCache.store(cacheId, raw)
+      await cache.raw.store(cacheId, raw)
     }
 
     const transformed = await transformer(raw)
 
     debugDoc('Transformation applied')
 
-    await transformedCache.store(cacheId, transformed)
+    await cache.transformed.store(cacheId, transformed)
 
     return transformed
   }
